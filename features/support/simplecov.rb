9ea5d8fe0894e52a06f266a897a9dc5727ad9fc4
Michael Dawson <mike@eskimodawn.com>
2013-02-20 16:37:01 -0500

    Remove useless file

--- features/support/simplecov.rb
+++
@@ -1,7 +0,0 @@
-# require 'simplecov'
-# SimpleCov.start 'rails'
-
-# # Screencast 6.8.1
-# # Also see https://github.com/colszowka/simplecov#using-simplecov-for-centralized-config
-
-# Zero LOC.  Let's try moving this to top of env.rb
