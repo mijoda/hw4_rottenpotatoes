23101a45bef155c9d50ab97ce767f1f83f8ebfde
Michael Dawson <mike@eskimodawn.com>
2013-02-20 09:09:35 -0800

    Comment out unused oddness method

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,7 @@
 module MoviesHelper
-  # Checks if a number is odd:
-  def oddness(count)
-    count.odd? ?  "odd" :  "even"
-  end
+  # Not used or tested
+  # # Checks if a number is odd:
+  # def oddness(count)
+  #   count.odd? ?  "odd" :  "even"
+  # end
 end
