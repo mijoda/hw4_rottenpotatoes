6e506fc85da0dfe05ac3d8569ed10a08ee1107f4
Michael Dawson <mike@eskimodawn.com>
2013-02-19 20:01:10 -0800

    Attempt to work cucumber into simplecov.  Results uncertain.

---
+++ features/support/simpleconv.rb
@@ -0,0 +1,4 @@
+# Screencast 6.8.1
+# Also see https://github.com/colszowka/simplecov#using-simplecov-for-centralized-config
+require 'simplecov'
+SimpleCov.start 'rails'
---
+++ spec/controllers/movie_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "MoviesController" do
+  describe "blarg" do
+    it 'should do whatever'
+  end
+end
